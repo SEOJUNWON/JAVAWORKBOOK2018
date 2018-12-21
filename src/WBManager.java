ca05a7686ec581a23cf0db3c83fcba90a8db6c1e
seojun-won <seojun-won@218.38.137.27>
2018-12-21 18:50:42 +0900

    1

--- src/WBManager.java
+++ src/WBManager.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-import workbook.StepA.StepAManager;
-import workbook.StepB.StepBManager;
-import workbook.StepC.StepCManager;
-import workbook.StepD.StepDManager;
+import workbook.StepA.stepAManager;
+import workbook.StepB.stepBManager;
+import workbook.StepC.stepCManager;
+import workbook.StepD.stepDManager;
 
 public class WBManager {
 
@@ -13,13 +13,13 @@ public class WBManager {
 		System.out.print("원하는 Step은?");
 		String step = s.next();
 		if(step.equalsIgnoreCase("A")) 
-			new StepAManager();
+			new stepAManager();
 		if(step.equalsIgnoreCase("B")) 
-			new StepBManager();
+			new stepBManager();
 		if(step.equalsIgnoreCase("C")) 
-			new StepCManager();
+			new stepCManager();
 		if(step.equalsIgnoreCase("D")) 
-			new StepDManager();
+			new stepDManager();
 		
 		System.out.printf("종료되었습니다.\n");
 	}	
