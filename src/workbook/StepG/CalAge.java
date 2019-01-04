3650e482e753dbbdffae8ef62fe9d900d03e6a8c
SEOJUNWON <40039780+SEOJUNWON@users.noreply.github.com>
2019-01-04 16:42:40 +0900

    Update CalAge.java

--- src/workbook/StepG/CalAge.java
+++ src/workbook/StepG/CalAge.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 public class CalAge {
 	public int birth_year;
 	public int age[] = new int[100];
-	public int count_person, count_baby, count_child; 
-	public int count_youth, count_young, count_adult;
-	public int count_old;
+	public int count[]=new int[7];
 	public int i;
+	String a[]= {"유아","어린이","청소년","청년","중년","노년"};
 	
 	public CalAge(){
 		input();
@@ -19,7 +18,7 @@ public class CalAge {
 			System.out.print((this.i+1)+"번째 사람의 태어난 년도를 입력하시오.  ");
 			this.birth_year = s.nextInt();
 			if(this.birth_year>2012) break;
-			this.count_person++;
+			this.count[0]++;
 			this.age[this.i]=2012-this.birth_year+1;
 			getAge();
 			
@@ -29,22 +28,22 @@ public class CalAge {
 	
 	void getAge() {
 		if(this.age[this.i]<7) {
-			this.count_baby++;
+			this.count[1]++;
 		}
 		else if(this.age[this.i]<13) {
-			this.count_child++;
+			this.count[2]++;
 		}
 		else if(this.age[this.i]<20) {
-			this.count_youth++;
+			this.count[3]++;
 		}
 		else if(this.age[this.i]<30) {
-			this.count_young++;
+			this.count[4]++;
 		}
 		else if(this.age[this.i]<60) {
-			this.count_adult++;
+			this.count[5]++;
 		}
 		else {
-			this.count_old++;
+			this.count[6]++;
 		}
 	}
 	
@@ -52,11 +51,9 @@ public class CalAge {
 		for(int j=0;j<this.i;j++) {
 			System.out.println((j+1)+"번째 사람의 나이는 "+this.age[j]+" 입니다. ");
 		}
-		System.out.println("유아는 "+count_baby+"명 입니다. ");
-		System.out.println("어린이는 "+count_child+"명 입니다. ");
-		System.out.println("청소년은 "+count_youth+"명 입니다. ");
-		System.out.println("청년은 "+count_young+"명 입니다. ");
-		System.out.println("중년은 "+count_adult+"명 입니다. ");
-		System.out.println("노년은 "+count_old+"명 입니다. ");
+		for(int j=0;j<6;j++) {
+			if(j>1)System.out.println(a[j]+"은 "+count[j+1]+"명 입니다. ");
+			else System.out.println(a[j]+"는 "+count[j+1]+"명 입니다. ");
+		}
 	}
 }
